ce071d0c42ade8d7eaa5275e805223e0e71ddce0
Till Kuhn <till.kuhn@dbschenker.com>
2020-06-18 00:38:30 +0200

    add svg

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private matIconRegistry: MatIconRegistry,private domSanitizer: DomSanitizer){
+    // https://www.digitalocean.com/community/tutorials/angular-custom-svg-icons-angular-material
+    this.matIconRegistry.addSvgIcon(
+      `backpack`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/backpack.svg')
+    );
+  }
   title = 'angkor-crud';
 }
