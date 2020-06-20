7c7c950a0efdbcf912cae539dc023fd242f54601
Till Kuhn <till.kuhn@dbschenker.com>
2020-06-20 10:36:03 +0200

    inject environment

--- ui/src/app/places/places.component.ts
+++ ui/src/app/places/places.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Place } from '../place';
+import { Environment } from '../environment';
 
 @Component({
   selector: 'app-places',
@@ -13,13 +14,13 @@ export class PlacesComponent implements OnInit {
   data: Place[] = [];
   isLoadingResults = true;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private env: Environment ) { }
 
   ngOnInit() {
     this.api.getPlaces()
       .subscribe((res: any) => {
         this.data = res;
-        console.log(this.data);
+        console.log('getPlaces()',this.env.version, this.data);
         this.isLoadingResults = false;
       }, err => {
         console.log(err);
