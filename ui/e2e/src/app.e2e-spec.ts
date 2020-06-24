84b74f623b88de455d5a0c15ac16688a22159772
Till Kuhn <till.kuhn@dbschenker.com>
2020-06-24 13:33:49 +0200

    lint and angkorize

--- ui/e2e/src/app.e2e-spec.ts
+++ ui/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular8-crud!');
+    expect(page.getTitleText()).toContain('Welcome to');
   });
 
   afterEach(async () => {
