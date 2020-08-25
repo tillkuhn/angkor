c07566f64b55aba7227710c2be447e4c1fe3df02
tillkuhn <kuhnibaert@gmail.com>
2020-08-25 18:09:40 +0200

    enhance certbot renew, add .terraform-version for tfswitch

--- infra/templates/deploy.sh
+++ infra/templates/deploy.sh
@@ -2,43 +2,77 @@
 # variables in this file are substitued by terraform templates
 # so you need to use double dollar signs ($$) to escape variables
 SCRIPT=$(basename $${BASH_SOURCE[0]})
-export WORKDIR=$(dirname $${BASH_SOURCE[0]})
-mkdir -p $${WORKDIR}/docs $${WORKDIR}/logs
+
+logit() {
+  printf "%(%Y-%m-%d %T)T %s\n" -1 "$1"
+}
 
 if [ $# -lt 1 ]; then
-    set -- help
+    set -- help # display help if called w/o args
 fi
 
 # common start
+export WORKDIR=$(dirname $${BASH_SOURCE[0]})
+mkdir -p $${WORKDIR}/docs $${WORKDIR}/logs
+
 # pull file artifacts needed for all targets from s3
 if [[ "$*" == *update* ]] || [[ "$*" == *all* ]]; then
+  logit "Updating docker-compose and script artifacts including myself"
   aws s3 cp s3://${bucket_name}/deploy/$${SCRIPT} $${WORKDIR}/$${SCRIPT} # update myself
   aws s3 cp s3://${bucket_name}/deploy/docker-compose.yml $${WORKDIR}/docker-compose.yml
   chmod ugo+x $${WORKDIR}/$${SCRIPT}
 fi
 
-# cerbot
+# cerbot renew
 if [[ "$*" == *cert* ]] || [[ "$*" == *all* ]]; then
+
+  # create daily cronjob job to execute check
+  if [ ! -f /etc/cron.daily/cerbot-renew ]; then
+    logit "Setting up job for certbot-renew in /etc/cron.daily"
+    sudo bash -c "cat >/etc/cron.daily/cerbot-renew" <<-'EOF'
+    /home/ec2-user/deploy.sh cert >>/home/ec2-user/logs/cerbot-renew.log 2>&1
+EOF
+    sudo chmod 755 /etc/cron.daily/cerbot-renew
+  fi
+
+  logit "Deploy and renew SSL Certificates"
+  CERTBOT_ADD_ARGS="--dry-run"
   if docker ps --no-trunc -f name=^/${appid}-ui$ |grep -q ${appid}; then
     echo ${appid}-ui is up, adding tempory shut down hook for cerbot renew
     set -x
-    sudo certbot --standalone -m ${certbot_mail} --agree-tos --expand --redirect -n ${certbot_domain_str} \
-                 --pre-hook "docker-compose stop ${appid}-ui" --post-hook "docker-compose start ${appid}-ui" certonly
+    sudo --preserve-env=WORKDIR certbot --standalone -m ${certbot_mail} --agree-tos --expand --redirect -n ${certbot_domain_str} \
+         --pre-hook "docker-compose --no-ansi --file $${WORKDIR}/docker-compose.yml stop ${appid}-ui" \
+         --post-hook "docker-compose --no-ansi --file $${WORKDIR}/docker-compose.yml start ${appid}-ui" \
+         $${CERTBOT_ADD_ARGS} certonly
     set +x
   else
     echo ${appid}-ui is down or not yet installed, cerbot can take safely over port 80
-    sudo certbot --standalone -m ${certbot_mail} --agree-tos --expand --redirect -n ${certbot_domain_str} $${certbot_hooks[@]} certonly
+    sudo --preserve-env=WORKDIR certbot --standalone -m ${certbot_mail} --agree-tos --expand --redirect -n ${certbot_domain_str}
+         $${CERTBOT_ADD_ARGS} certonly
   fi
+
+  # if files relevant to letsencrypt changed, trigger backup update
+  if sudo find /etc/letsencrypt/ -mtime -1 |grep -q "."; then
+    logit "Files in /etc/letsencrypt changes, trigger backup"
+    sudo tar -C /etc -zcf /tmp/letsencrypt.tar.gz letsencrypt
+    sudo aws s3 cp --sse=AES256 /tmp/letsencrypt.tar.gz s3://${bucket_name}/backup/letsencrypt.tar.gz
+    sudo rm -f /tmp/letsencrypt.tar.gz
+  else
+    logit "Files in /etc/letsencrypt are unchanged, skip backup"
+  fi
+
 fi
 
-# python webhook
+# python or golang webhook
 if [[ "$*" == *webhook* ]] || [[ "$*" == *all* ]]; then
+  logit "Deploying webhook"
   aws s3 cp s3://${bucket_name}/deploy/captain-hook.py $${WORKDIR}/captain-hook.py
   chmod ugo+x $${WORKDIR}/captain-hook.py
 fi
 
 # antora docs
 if [[ "$*" == *docs* ]] || [[ "$*" == *all* ]]; then
+  logit "Deploying Antora docs"
   set -x
   aws s3 sync --delete s3://${bucket_name}/docs $${WORKDIR}/docs/
   set +x
@@ -46,12 +80,14 @@ fi
 
 # api deployment
 if [[ "$*" == *api* ]] || [[ "$*" == *all* ]]; then
+  logit "Deploying API Backend"
   # pull recent docker images from dockerhub
   docker pull ${docker_user}/${appid}-api:${api_version}
   docker-compose --file $${WORKDIR}/docker-compose.yml up --detach ${appid}-api
 fi
 
 if [[ "$*" == *ui* ]] || [[ "$*" == *all* ]]; then
+  logit "Deploying UI Frontend"
   docker pull ${docker_user}/${appid}-ui:${ui_version}
   docker-compose --file $${WORKDIR}/docker-compose.yml up --detach ${appid}-ui
 fi
