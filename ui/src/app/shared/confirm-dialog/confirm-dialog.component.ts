f78bc152fb4110c49313e97a199e42c25cfceeff
tillkuhn <kuhnibaert@gmail.com>
2020-08-25 22:23:55 +0200

    introduce delete confirm dialogue

---
+++ ui/src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,42 @@
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+
+/**
+ * Class to represent confirm dialog model.
+ *
+ * It has been kept here to keep it as part of shared component.
+ */
+export class ConfirmDialogModel {
+
+  constructor(public title: string, public message: string) {
+  }
+}
+
+// https://onthecode.co.uk/create-confirm-dialog-angular-material
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss']
+})
+export class ConfirmDialogComponent {
+  title: string;
+  message: string;
+
+  constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogModel) {
+    // Update view with given values
+    this.title = data.title;
+    this.message = data.message;
+  }
+
+  onConfirm(): void {
+    // Close the dialog, return true
+    this.dialogRef.close(true);
+  }
+
+  onDismiss(): void {
+    // Close the dialog, return false
+    this.dialogRef.close(false);
+  }
+}
+
