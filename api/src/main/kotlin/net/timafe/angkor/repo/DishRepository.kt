bf4962e38aaa241325e4eb0792a8fbfa19bdedd3
tillkuhn <kuhnibaert@gmail.com>
2020-09-29 09:20:27 +0200

    introduce authscoped queries

--- api/src/main/kotlin/net/timafe/angkor/repo/DishRepository.kt
+++ api/src/main/kotlin/net/timafe/angkor/repo/DishRepository.kt
@@ -1,7 +1,10 @@
 package net.timafe.angkor.repo
 
 import net.timafe.angkor.domain.Dish
+import net.timafe.angkor.domain.enums.AuthScope
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import java.util.*
 
 interface DishRepository : CrudRepository<Dish, UUID> {
@@ -9,5 +12,8 @@ interface DishRepository : CrudRepository<Dish, UUID> {
     fun findByName(name: String): List<Dish>
 
     override fun findAll(): List<Dish>
+
+    @Query("SELECT d FROM Dish d WHERE d.authScope IN (:authScopes)")
+    fun findDishesByAuthScope(@Param("authScopes") authScopes: List<AuthScope>): List<Dish>
 
 }
--- api/src/main/kotlin/net/timafe/angkor/rest/DishController.kt
+++ api/src/main/kotlin/net/timafe/angkor/rest/DishController.kt
@@ -2,6 +2,7 @@ package net.timafe.angkor.rest
 
 import net.timafe.angkor.config.Constants
 import net.timafe.angkor.domain.Dish
+import net.timafe.angkor.domain.enums.AuthScope
 import net.timafe.angkor.repo.DishRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -30,6 +31,15 @@ class DishController {
         val dishes = dishRepository.findAll()
         //  coo ${places.get(0).coordinates}"
         log.info("allDishes() return ${dishes.size} dishes principal=${principal}")
+        return dishes
+    }
+
+    @GetMapping("scoped")
+    @ResponseStatus(HttpStatus.OK)
+    fun allDishesByAuthScope(): List<Dish> {
+        val authScopes = listOf(AuthScope.PUBLIC)
+        val dishes = dishRepository.findDishesByAuthScope(authScopes)
+        log.info("findDishesByAuthScope() return ${dishes.size} dishes authScopes=${authScopes}")
         return dishes
     }
 
