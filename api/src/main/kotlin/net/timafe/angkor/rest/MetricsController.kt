6d924cef73f321ebd08cddc81fafa40c5bb8c015
tillkuhn <kuhnibaert@gmail.com>
2020-10-05 15:56:15 +0200

    use filter / map

--- api/src/main/kotlin/net/timafe/angkor/rest/MetricsController.kt
+++ api/src/main/kotlin/net/timafe/angkor/rest/MetricsController.kt
@@ -8,7 +8,6 @@ import org.springframework.boot.actuate.metrics.MetricsEndpoint
 import org.springframework.core.SpringVersion
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 /**
  * https://stackoverflow.com/questions/32382349/how-to-get-metrics-from-spring-boot-actuator-programmatically
  */
-class MetricsController(private val metrics: MetricsEndpoint, private val objectMapper: ObjectMapper) {
+class MetricsController(private val metricsEndpoint: MetricsEndpoint, private val objectMapper: ObjectMapper) {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -42,24 +41,23 @@ class MetricsController(private val metrics: MetricsEndpoint, private val object
     @ResponseStatus(HttpStatus.OK)
     fun metrics(): List<MetricDTO> {
         val meli = mutableListOf<MetricDTO>()
-        meli.add(MetricDTO("spring.version", "Spring Framework Version",SpringVersion.getVersion(),null))
-        meli.add(MetricDTO("java.version", "Java Major Minor Version",System.getProperty("java.version"),null))
-        meli.add(MetricDTO("kotlin.version", "Kotlin Version",KotlinVersion.CURRENT.toString(),null))
-        metrics.listNames().names.filter { filterNames.contains(it) }
-                .forEach {
-                    // example it: tomcat.sessions.active.current
-                    val resp: MetricsEndpoint.MetricResponse = metrics.metric(it, null)
-                    // log.trace(objectMapper.writeValueAsString(resp))
-                    val metricDTO = MetricDTO(resp.name, resp.description, resp.measurements.get(0).value.toString(), resp.baseUnit)
-                    meli.add(metricDTO)
+        meli.add(MetricDTO("spring.version", "Spring Framework Version", SpringVersion.getVersion(), null))
+        meli.add(MetricDTO("java.version", "Java Major Minor Version", System.getProperty("java.version"), null))
+        meli.add(MetricDTO("kotlin.version", "Kotlin Version", KotlinVersion.CURRENT.toString(), null))
+        meli.addAll(metricsEndpoint.listNames().names
+                .filter { filterNames.contains(it) }
+                .map {
+                    val resp: MetricsEndpoint.MetricResponse = metricsEndpoint.metric(it, null)
+                    MetricDTO(resp.name, resp.description, resp.measurements.get(0).value.toString(), resp.baseUnit)
                 }
+        )
         return meli
     }
 
-/*    @GetMapping("${Constants.API_DEFAULT_VERSION}/metrics")
+    @GetMapping("${Constants.API_DEFAULT_VERSION}/metrics")
     @ResponseStatus(HttpStatus.OK)
     fun publicMetrics(): List<MetricDTO> {
         return this.metrics()
-    }*/
+    }
 
 }
