ca98c6f1efae625f445a3afe65e1496602f1f34c
tillkuhn <kuhnibaert@gmail.com>
2020-10-27 20:12:18 +0100

    restore main class

--- api/src/main/kotlin/net/timafe/angkor/Application.kt
+++ api/src/main/kotlin/net/timafe/angkor/Application.kt
@@ -12,11 +12,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EnableJpaRepositories
 @EnableJpaAuditing // https://www.baeldung.com/database-auditing-jpa
 @EnableConfigurationProperties(AppProperties::class)
+class Application
 
-class Application {
-
-    fun main(args: Array<String>) {
-        SpringApplication.run(Application::class.java, *args)
-    }
-    
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
 }
+
