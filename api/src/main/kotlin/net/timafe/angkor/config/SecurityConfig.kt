9d1955d68b333cba089d3d4baef646ca2765c347
tillkuhn <kuhnibaert@gmail.com>
2020-10-29 08:07:19 +0100

    expose health endpoint for healthchecks

--- api/src/main/kotlin/net/timafe/angkor/config/SecurityConfig.kt
+++ api/src/main/kotlin/net/timafe/angkor/config/SecurityConfig.kt
@@ -5,11 +5,12 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 
 @Configuration
 @EnableWebSecurity
 // @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class  SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
     public override fun configure(http: HttpSecurity) {
@@ -33,14 +34,12 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 .antMatchers(HttpMethod.DELETE, "/api/v1/places/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/v1/places/**").hasRole("USER")
                 .antMatchers(HttpMethod.POST, "/api/v1/places/**").hasRole("USER")
-                //.antMatchers("/management/**").hasAuthority(ADMIN)
                 .and()
+
+                // Configures authentication support using an OAuth 2.0 and/or OpenID Connect 1.0 Provider.
                 .oauth2Login()
                 .and()
-                //.oauth2ResourceServer()
-                //.jwt()
-                //.jwtAuthenticationConverter(jwtAuthorityExtractor)
-                //.and()
+                //  Configures OAuth 2.0 Client support.
                 .oauth2Client()
     }
 }
