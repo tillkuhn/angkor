937d7c8ec0045cf0f7ea05fafa733f0e26f9a170
tillkuhn <kuhnibaert@gmail.com>
2020-11-04 20:59:40 +0100

    dockercompose is no longer a template, switch to .env approach

--- infra/file-resources.tf
+++ infra/file-resources.tf
@@ -1,25 +1,8 @@
 # remote files
 resource "aws_s3_bucket_object" "dockercompose" {
-  bucket = module.s3.bucket_name
-  key    = "deploy/docker-compose.yml"
-  content = templatefile("${path.module}/templates/docker-compose.yml", {
-    appid                = var.appid
-    db_url               = var.db_url
-    db_username          = var.db_username
-    db_password          = var.db_password
-    api_version          = var.api_version
-    ui_version           = var.ui_version
-    docker_user          = var.docker_user
-    certbot_domain_name  = var.certbot_domain_name
-    mapbox_access_token  = var.mapbox_access_token
-    imprint_url          = var.imprint_url
-    oauth2_client_id     = module.cognito.app_client_id
-    oauth2_client_name   = module.cognito.app_client_name
-    oauth2_client_secret = module.cognito.app_client_secret
-    oauth2_issuer_uri    = module.cognito.pool_issuer_uri
-    server_names = join(" ", concat([
-    var.certbot_domain_name], var.certbot_subject_alterntive_names))
-  })
+  bucket        = module.s3.bucket_name
+  key           = "deploy/docker-compose.yml"
+  content       = file("${path.module}/files/docker-compose.yml")
   storage_class = "REDUCED_REDUNDANCY"
 }
 
@@ -33,24 +16,30 @@ resource "aws_s3_bucket_object" "deployscript" {
 
 locals {
   dotenv_content = templatefile("${path.module}/templates/.env", {
-    appid               = var.appid
-    ACCOUNT_ID          = module.vpcinfo.account_id
-    AWS_REGION          = module.vpcinfo.aws_region
-    ssh_privkey_file    = pathexpand(var.ssh_privkey_file)
-    bucket_name         = module.s3.bucket_name
-    instance_id         = module.ec2.instance.id
-    public_ip           = module.ec2.instance.public_ip
-    db_url              = var.db_url
-    db_username         = var.db_username
-    db_password         = var.db_password
-    api_version         = var.api_version
-    ui_version          = var.ui_version
-    docker_token        = var.docker_token
-    docker_user         = var.docker_user
-    certbot_mail        = var.certbot_mail
-    certbot_domain_name = var.certbot_domain_name
-    certbot_domain_str = format("-d %s", join(" -d ", concat([
-    var.certbot_domain_name], var.certbot_subject_alterntive_names)))
+    ACCOUNT_ID           = module.vpcinfo.account_id
+    AWS_REGION           = module.vpcinfo.aws_region
+    api_version          = var.api_version
+    appid                = var.appid
+    bucket_name          = module.s3.bucket_name
+    certbot_domain_name  = var.certbot_domain_name
+    certbot_domain_str   = format("-d %s", join(" -d ", concat([var.certbot_domain_name], var.certbot_subject_alterntive_names)))
+    certbot_mail         = var.certbot_mail
+    db_password          = var.db_password
+    db_url               = var.db_url
+    db_username          = var.db_username
+    docker_token         = var.docker_token
+    docker_user          = var.docker_user
+    imprint_url          = var.imprint_url
+    instance_id          = module.ec2.instance.id
+    mapbox_access_token  = var.mapbox_access_token
+    oauth2_client_id     = module.cognito.app_client_id
+    oauth2_client_name   = module.cognito.app_client_name
+    oauth2_client_secret = module.cognito.app_client_secret
+    oauth2_issuer_uri    = module.cognito.pool_issuer_uri
+    public_ip            = module.ec2.instance.public_ip
+    server_names         = join(" ", concat([var.certbot_domain_name], var.certbot_subject_alterntive_names))
+    ssh_privkey_file     = pathexpand(var.ssh_privkey_file)
+    ui_version           = var.ui_version
   })
 }
 
