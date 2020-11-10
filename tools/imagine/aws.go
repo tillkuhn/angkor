650d27db43f47706e4f182b6c744edea7b7da512
tillkuhn <kuhnibaert@gmail.com>
2020-11-10 13:50:07 +0100

    add comments§

--- tools/imagine/aws.go
+++ tools/imagine/aws.go
@@ -1,28 +1,3 @@
-/*
- snippet from https://gist.github.com/hiyali/6e3dd0b17d44ae92c1222011d6a6df8f
-
-  https://docs.aws.amazon.com/cli/latest/reference/s3api/put-object-acl.html
-  https://docs.aws.amazon.com/zh_cn/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html
-  https://github.com/awslabs/aws-go-wordfreq-sample/blob/master/cmd/uploads3/main.go
-  https://docs.aws.amazon.com/sdk-for-go/api/aws/
-
-  1. Create bucket in s3 & get the keys
-  - login to UI web aws s3 interface
-  - go to S3 service
-  - create a Bucket called SomeBucket in the desired region
-  2. Install dependencies & configure aws keys
-  - sudo apt install awscli
-  - first configure your aws credentials run: aws configure
-  - go get -u github.com/aws/aws-sdk-go/aws
-  - go get -u github.com/hiyali/logli (for logging)
-  3. Configure this tool & run
-  - configure constants (S3_**) of this file for your s3 bucket
-  - go run syncer.go filename s3-filepath (e.g.: go run syncer.go ../downloads/Beyoncé/Beyoncé-Halo.mp3 Beyoncé/Halo.mp3)
-*/
-/* Enjoy the codes (github.com/hiyali)
-   go build -o syncer (ONCE)
-   syncer filename s3-filepath
-*/
 package main
 
 import (
@@ -44,6 +19,9 @@ type S3Handler struct {
 	KeyPrefix  string
 }
 
+/**
+ * Put new object into bucket
+ */
 func (h S3Handler) UploadFile(key string, filename string) error {
 	file, err := os.Open(filename)
 	if err != nil {
