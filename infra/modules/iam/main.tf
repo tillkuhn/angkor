a978258bc7237b192b179e89a06e603dc7eae3ec
tillkuhn <kuhnibaert@gmail.com>
2020-11-11 20:47:39 +0100

    extend policy to support s3 object tags

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -53,7 +53,7 @@ resource "aws_iam_role_policy" "instance_policy" {
         "s3:GetBucketLocation",
         "s3:GetObject",
         "s3:ListBucket",
-        "s3:PutObject"
+        "s3:PutObject*"
       ],
       "Resource": [ "arn:aws:s3:::${var.bucket_name}/*" ]
     },
