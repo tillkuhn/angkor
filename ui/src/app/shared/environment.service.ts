4da63b1791508d3e26435cfea0bdea39842ff27b
tillkuhn <kuhnibaert@gmail.com>
2021-01-31 11:05:01 +0100

    support release name in uI

--- ui/src/app/shared/environment.service.ts
+++ ui/src/app/shared/environment.service.ts
@@ -10,6 +10,7 @@ import {Injectable, VERSION} from '@angular/core';
 export class EnvironmentService {
 
   appVersion: string;
+  releaseName; string;
   mapboxAccessToken: string;
   imprintUrl: string;
   angularVersion = VERSION.full; // e.g. 10.0.7 see https://github.com/angular/angular/issues/1357#issuecomment-346084639
@@ -20,6 +21,7 @@ export class EnvironmentService {
     this.appVersion = windowEnv && windowEnv.APP_VERSION !== '' && windowEnv.APP_VERSION !== '${APP_VERSION}' ? windowEnv.APP_VERSION : 'latest';
     this.mapboxAccessToken = windowEnv && windowEnv.MAT !== '' && windowEnv.MAT !== '${MAT}' ? windowEnv.MAT : 'no-token';
     this.imprintUrl = windowEnv && windowEnv.IMPRINT_URL !== '' && windowEnv.IMPRINT_URL !== '${IMPRINT_URL}' ? windowEnv.IMPRINT_URL : '404';
+    this.releaseName = windowEnv && windowEnv.RELEASE_NAME !== '' && windowEnv.RELEASE_NAME !== '${RELEASE_NAME}' ? windowEnv.RELEASE_NAME : 'latest';
   }
 
 }
--- ui/src/assets/window-env.tmpl.js
+++ ui/src/assets/window-env.tmpl.js
@@ -3,6 +3,7 @@
 // make sure this file is also considered as asset in angular.json
 window.env = {
   APP_VERSION: '${APP_VERSION}',
+  RELEASE_NAME: '${RELEASE_NAME}',
   MAT: '${MAPBOX_ACCESS_TOKEN}',
   IMPRINT_URL: '${IMPRINT_URL}'
 };
