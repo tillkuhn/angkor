aca44081c86276b9cd22a7dc19936f74936d0cbc
tillkuhn <kuhnibaert@gmail.com>
2021-03-31 15:30:30 +0200

    prepare for RSS interface

---
+++ api/src/main/kotlin/net/timafe/angkor/domain/dto/Feed.kt
@@ -0,0 +1,19 @@
+package net.timafe.angkor.domain.dto
+
+/**
+ * JSON Representation of RSS Feed
+ * based on https://diamantidis.github.io/2019/10/13/json-feed-reader-app-with-kotlin-native
+ */
+data class Feed (
+    val title: String?,
+    // @SerialName("home_page_url")
+    val homePageURL: String,
+    // @SerialName("feed_url")
+    var feedURL: String,
+    val description: String,
+    val icon: String? = null,
+    val favicon: String? = null,
+    var expired: Boolean? = null,
+    val author: String, // Author
+    val items: List<FeedItem>
+)
