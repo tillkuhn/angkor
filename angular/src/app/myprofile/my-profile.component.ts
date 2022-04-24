2c7315f6c067247715b651304258c0ea6bb6ffc8
tillkuhn <kuhnibaert@gmail.com>
2022-04-24 13:23:25 +0200

    #268 Allow users to request data deletion part 1

--- angular/src/app/myprofile/my-profile.component.ts
+++ angular/src/app/myprofile/my-profile.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {AuthService} from '@shared/services/auth.service';
+import {ConfirmDialogComponent, ConfirmDialogModel, ConfirmDialogResult} from '@shared/components/confirm-dialog/confirm-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+import {NotificationService} from '@shared/services/notification.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -9,7 +12,24 @@ import {AuthService} from '@shared/services/auth.service';
 export class MyProfileComponent {
 
 
-  constructor(public authService: AuthService) {
-  }
+  constructor(public authService: AuthService,
+              private dialog: MatDialog,
+              private notifications: NotificationService,
+  ) {}
 
+  confirmRequestRemoveDialog(): void {
+    const message = `Are you sure you want all your data to be removed?`;
+    const dialogData = new ConfirmDialogModel('Confirm Action', message);
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '400px',
+      data: dialogData
+    });
+
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      if ((dialogResult as ConfirmDialogResult).confirmed) {
+        // Removal request has been successfully sent and will be processed asap
+        this.notifications.success('Removal request to be fully implemented soon!');
+      }
+    });
+  }
 }
