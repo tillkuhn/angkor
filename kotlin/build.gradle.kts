93e3a4ed9652bc6aa40d3e27d2925cea3119b2ce
tillkuhn <kuhnibaert@gmail.com>
2024-01-02 14:34:26 +0100

    fix deprecated api

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -221,7 +221,7 @@ tasks.register("bootRunProd") {
 // https://github.com/ben-manes/gradle-versions-plugin#tasks
 // disallow release candidates as upgradable versions from stable versions
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
