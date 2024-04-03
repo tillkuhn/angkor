f70ed5a2bae0dadc90068b43af88dd13029408fa
tillkuhn <kuhnibaert@gmail.com>
2024-04-03 17:55:12 +0200

    make sure KAFKA_SASL_USERNAME is set to prevent fatal error during topkapi unit tests

--- go/topkapi/client_test.go
+++ go/topkapi/client_test.go
@@ -1,36 +1,37 @@
 package topkapi
 
 import (
-    "fmt"
-    "hash/adler32"
-    "os"
-    "testing"
+	"fmt"
+	"hash/adler32"
+	"os"
+	"testing"
 )
 
 func TestTopicPrefix(t *testing.T) {
-    config := &ClientConfig{
-        TopicPrefix: "hase-",
-    }
-    fulltopic := getTopicWithPrefix("horst", config)
-    if fulltopic != "hase-horst" {
-        t.Error(fulltopic + " unexpected")
-    }
+	config := &ClientConfig{
+		TopicPrefix: "hase-",
+	}
+	fullTopic := getTopicWithPrefix("horst", config)
+	if fullTopic != "hase-horst" {
+		t.Error(fullTopic + " unexpected")
+	}
 }
 
 func TestChecksum(t *testing.T) {
-    message := "SummerOf69!"
-    checksum := adler32.Checksum([]byte(message))
-    if checksum != 422708159 {
-        t.Error(fmt.Sprint(checksum) + " unexpected")
-    }
+	message := "SummerOf69!"
+	checksum := adler32.Checksum([]byte(message))
+	if checksum != 422708159 {
+		t.Error(fmt.Sprint(checksum) + " unexpected")
+	}
 }
 
 func TestClientSetup(t *testing.T) {
-    _ = os.Setenv("KAFKA_BROKERS", "localhost:9094")
-    expected := "testTheRest"
-    client := NewClientWithId(expected)
-    actual1 := client.Config.ClientId
-    if actual1 != expected {
-        t.Error(fmt.Sprintf("Expected %s got %s", expected, actual1))
-    }
+	_ = os.Setenv("KAFKA_BROKERS", "localhost:9094")
+	_ = os.Setenv("KAFKA_SASL_USERNAME", "hasentiger123")
+	expected := "testTheRest"
+	client := NewClientWithId(expected)
+	actual1 := client.Config.ClientId
+	if actual1 != expected {
+		t.Error(fmt.Sprintf("Expected %s got %s", expected, actual1))
+	}
 }
