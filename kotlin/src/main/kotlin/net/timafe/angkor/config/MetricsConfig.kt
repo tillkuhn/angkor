c0e2b39963b2d75fd2ae52fe83745a7cd398a340
Till Kuhn <tillkuhn@users.noreply.github.com>
2025-12-31 12:37:56 +0100

    add support for jdk8 datatypes (jackson) explicity, fix new import path for MeterRegistryCustomizer

--- kotlin/src/main/kotlin/net/timafe/angkor/config/MetricsConfig.kt
+++ kotlin/src/main/kotlin/net/timafe/angkor/config/MetricsConfig.kt
@@ -4,7 +4,7 @@ import io.micrometer.core.instrument.Meter
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.config.MeterFilter
 import io.micrometer.core.instrument.config.MeterFilterReply
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
+import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
